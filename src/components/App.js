556bae6ca19e34ca1d85474f04143eb4ca892aa4
garrettvercoe <garrettvercoe@gmail.com>
2019-08-09 12:25:09 -0400

    Pruned Npm Package

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
-import { Stage, Layer, Rect, Text, Circle } from "react-konva";
-import Konva from "konva";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Stage } from "react-konva";
 import Portal from "./Portal";
 import BottomPlayback from "./BottomPlayback";
 import MountedTones from "./MountedTones";
-import { connect } from "react-redux";
 import reducer from "../reducers";
 import middleware from "../middleware";
 import { Provider } from "react-redux";
